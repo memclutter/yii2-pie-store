f0071ae4b946bad4c3368e531e2cf08b8a39f314
Memory Clutter <memclutter@gmail.com>
2016-09-03 17:50:04 +0300

    Small fixes

--- app/config/web.php
+++ app/config/web.php
@@ -8,6 +8,7 @@ $config = [
     'bootstrap' => ['log'],
     'defaultRoute' => 'product/index',
     'homeUrl' => '/',
+    'language' => 'ru-RU',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
